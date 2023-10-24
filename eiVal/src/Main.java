3a2d03203a92bc578e80f51fc1896d9cc4b8fb33
TimDep <tim.depickere@student.kuleuven.be>
2023-10-24 09:47:55 +0200

    update

--- eiVal/src/Main.java
+++ eiVal/src/Main.java
@@ -26,6 +26,6 @@ public class Main {
                 }
             }
         }
-        System.out.println(Arrays.deepToString(rooster));;
+        System.out.println(Arrays.deepToString(rooster));
     }
 }
